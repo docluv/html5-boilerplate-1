71a4d1474be191e0dcb63d37e9b0fd4957a330da
Chris Love <clove@extremewebworks.com>
2017-07-18 17:43:21 -0400

    updated to precache assets

--- dist/sw.js
+++ dist/sw.js
@@ -1,6 +1,45 @@
+const preCacheName = "pre-cache-hbp",
+    preCacheFiles = [
+        "/",
+        "css/normalize.css",
+        "css/main.css",
+        "img/html5boilerplate-152x152.png",
+        "js/vendor/modernizr-3.5.0.min.js",
+        "https://code.jquery.com/jquery-3.2.1.min.js",
+        "js/plugins.js",
+        "js/main.js"
+    ];
 
-self.addEventListener("fetch", function(evt){
 
-    console.log("fetch: ", evt);
+self.addEventListener("install", event => {
+
+    console.log("installing precache files");
+
+    caches.open(preCacheName).then(function (cache) {
+
+        return cache.addAll(preCacheFiles);
+
+    });
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request).then(response => {
+
+            if (!response) {
+
+                //fall back to the network fetch
+                return fetch(event.request);
+
+            }
+
+            return response;
+
+        })
+
+    )
 
 });
